80777f675aa46313b3d9afe2eb1ef08e7313639c
byjulie0 <julievieiramartins0@gmail.com>
2025-09-17 15:25:17 -0400

    Aula 1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper></div>
+    </div>
+
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
+    <h1>Página de Libras</h1>
+    <img src="coelho.jpg" alt="coelho">
+    
+</body>
+</html>
